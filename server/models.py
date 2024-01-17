600b5227a20c221fa0c1753d63062d13aaeeb166
faithwest <faithchepkoech425@gmail.com>
2024-01-17 12:27:58 +0300

    completed assignment,fixed proxy errors,and cors errors

--- server/models.py
+++ server/models.py
@@ -1,25 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
-from sqlalchemy import Column, String, DECIMAL, Integer
-
 
 db = SQLAlchemy()
 
 class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
-    
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    image = Column(String)
-    price = Column(DECIMAL)
 
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.Float)
 
-'''
-     SUCCESFUL MIGRATIONS
-     INFO/nINFO/nINFO
-
-      DB ERRORS
-many lines of errors starting with file:check imports,syntax errors 
-error with info/DDL and target dtabase>>check history then proceed to upgrade
-error starting with usage:....or could not import app>>wrong directory
-'''
+    def __repr__(self):
+        return f'<Plant {self.name} | Price: {self.price}>'
